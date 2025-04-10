6c6e68e9da99cbfdc61812859ce075d38f266788
Jinozzs <ozzs0226@gmail.com>
2025-04-10 19:58:39 +0900

    프로젝트 생성

---
+++ ggjh/ggjh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ggjh
+//
+//  Created by Seoyeong on 4/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ggjh/ggjh/ggjhApp.swift
@@ -0,0 +1,17 @@
+//
+//  ggjhApp.swift
+//  ggjh
+//
+//  Created by Seoyeong on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ggjhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
