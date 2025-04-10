bfc18756f062ff98c9cb18620657dc31ab88b310
Jinozzs <132433301+Jinozzs@users.noreply.github.com>
2025-04-10 20:08:38 +0900

    줄리엔 바보

--- ggjh/ggjh/ContentView.swift
+++ ggjh/ggjh/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello❤️!")
+            Text("줄리엔 바보🤪")
         }
         .padding()
     }
